be01a9f7b90fe85be99e3b8a34f0c09a5b78d712
Mickael VERMAND <mickael.vermand@gmail.com>
2024-09-30 07:22:28 +0200

    Added form validation, split login and register form

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -35,83 +35,10 @@
                         </li>
                     </ul>
 
-                    <!-- Login Form -->
-                    <form v-if="tab == 'login'">
-                        <!-- Email -->
-                        <div class="mb-3">
-                            <label class="inline-block mb-2">Email</label>
-                            <input type="email"
-                                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                                placeholder="Enter Email" />
-                        </div>
-                        <!-- Password -->
-                        <div class="mb-3">
-                            <label class="inline-block mb-2">Password</label>
-                            <input type="password"
-                                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                                placeholder="Password" />
-                        </div>
-                        <button type="submit"
-                            class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700">
-                            Submit
-                        </button>
-                    </form>
-                    <!-- Registration Form -->
-                    <form v-if="tab != 'login'">
-                        <!-- Name -->
-                        <div class="mb-3">
-                            <label class="inline-block mb-2">Name</label>
-                            <input type="text"
-                                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                                placeholder="Enter Name" />
-                        </div>
-                        <!-- Email -->
-                        <div class="mb-3">
-                            <label class="inline-block mb-2">Email</label>
-                            <input type="email"
-                                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                                placeholder="Enter Email" />
-                        </div>
-                        <!-- Age -->
-                        <div class="mb-3">
-                            <label class="inline-block mb-2">Age</label>
-                            <input type="number"
-                                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded" />
-                        </div>
-                        <!-- Password -->
-                        <div class="mb-3">
-                            <label class="inline-block mb-2">Password</label>
-                            <input type="password"
-                                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                                placeholder="Password" />
-                        </div>
-                        <!-- Confirm Password -->
-                        <div class="mb-3">
-                            <label class="inline-block mb-2">Confirm Password</label>
-                            <input type="password"
-                                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                                placeholder="Confirm Password" />
-                        </div>
-                        <!-- Country -->
-                        <div class="mb-3">
-                            <label class="inline-block mb-2">Country</label>
-                            <select
-                                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded">
-                                <option value="USA">USA</option>
-                                <option value="Mexico">Mexico</option>
-                                <option value="Germany">Germany</option>
-                            </select>
-                        </div>
-                        <!-- TOS -->
-                        <div class="mb-3 pl-6">
-                            <input type="checkbox" class="w-4 h-4 float-left -ml-6 mt-1 rounded" />
-                            <label class="inline-block">Accept terms of service</label>
-                        </div>
-                        <button type="submit"
-                            class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700">
-                            Submit
-                        </button>
-                    </form>
+                    <login-form v-if="tab == 'login'"></login-form>
+                    <register-form v-else></register-form>
+
+
                 </div>
             </div>
         </div>
@@ -121,8 +48,15 @@
 <script>
 import useModalStore from "@/stores/modal"
 import { mapState, mapWritableState } from "pinia";
+import LoginForm from '@/components/LoginForm.vue'
+import RegisterForm from '@/components/RegisterForm.vue'
+
 export default {
     name: 'AppAuth',
+    components: {
+        LoginForm,
+        RegisterForm
+    },
     computed: {
         ...mapState(useModalStore, ["hiddenClass"]),
         ...mapWritableState(useModalStore, { // Using alias to avid renaming all isOpen
@@ -131,13 +65,15 @@ export default {
     },
     data() {
         return {
-            tab: "login"
+            tab: "login",
         }
     },
     methods: {
         toggleAuthModal() {
             this.modalVisibility = !this.modalVisibility
-        }
+        },
+
+
     }
 }
 </script>
---
+++ src/includes/validation.js
@@ -0,0 +1,66 @@
+import {
+  Form as VeeForm,
+  Field as VeeField,
+  defineRule,
+  ErrorMessage,
+  configure
+} from 'vee-validate'
+import {
+  required,
+  min,
+  max,
+  alpha_spaces as alphaSpaces,
+  email,
+  min_value as minValue,
+  max_value as maxValue,
+  confirmed,
+  not_one_of as excluded
+} from '@vee-validate/rules'
+
+export default {
+  install(app) {
+    app.component('VeeForm', VeeForm)
+    app.component('VeeField', VeeField)
+    app.component('ErrorMessage', ErrorMessage)
+    //Import existing rules from package
+    defineRule('required', required)
+    defineRule('min', min)
+    defineRule('max', max)
+    defineRule('alpha_spaces', alphaSpaces)
+    defineRule('email', email)
+    defineRule('min_value', minValue)
+    defineRule('max_value', maxValue)
+    defineRule('password_mismatchs', confirmed)
+    defineRule('excluded', excluded)
+    defineRule('country_excluded', excluded) // To create a specific case for country input
+    defineRule('tos', required)
+
+    configure({
+      generateMessage: (ctx) => {
+        const messages = {
+          required: `The field ${ctx.field} is required.`,
+          min: `The field ${ctx.field} is too short.`,
+          max: `The field ${ctx.field} is too long.`,
+          alpha_spaces: `The field ${ctx.field} may only contain alphabetical characters and spaces.`,
+          email: `The field ${ctx.field} must be a valid email.`,
+          min_value: `The field ${ctx.field} is too low.`,
+          max_value: `The field ${ctx.field} is too high.`,
+          excluded: `You are not allow to use this value for the field ${ctx.field}.`,
+          country_excluded: `Due to restrictions, we do not accept users from this location.`,
+          password_mismatchs: `Passwords don't match`,
+          tos: `You must accept the Terms of Service`
+        }
+
+        const message = messages[ctx.rule.name]
+          ? messages[ctx.rule.name]
+          : `The field ${ctx.field} is not valid`
+
+        return message
+      },
+      validateOnBlur: true,
+      validateOnChange: true,
+      validateOnInput: false,
+      validateOnModelUpdate: true
+    })
+  }
+}
